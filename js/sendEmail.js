f5a00c61044e54b45c56d25b8fa81f26e799a66b
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-05-21 18:25:09 +0100

    added sendEmailjs

---
+++ js/sendEmail.js
@@ -0,0 +1,23 @@
+function sendMail(contactForm ) {
+    
+    emailjs.send("gmail", "ftt", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    contactForm.reset();
+    return false;  // To block from loading a new page
+
+
+}
+
+
+   
