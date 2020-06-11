f2208e736e8321520231a1ef36dee85ae0bb4ae9
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-06-11 23:52:12 +0100

    Deleted customFormValidation.js file

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,9 +1,9 @@
-function sendMail(contactForm ) {
-    
+function sendMail(name, email, message) {
+   
     emailjs.send("gmail", "ftt", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "message": contactForm.message.value
+        "from_name": name.value,
+        "from_email": email.value,
+        "message": message.value
     })
     .then(
         function(response) {
@@ -13,11 +13,55 @@ function sendMail(contactForm ) {
             console.log("FAILED", error);
         }
     );
-    contactForm.reset();
+    
+    // contactForm.reset();
     return false;  // To block from loading a new page
+};
+    
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const msg = document.getElementById('msg');
+const sendAction = document.getElementById('sendMessageButton');
 
+sendAction.addEventListener('click', function (e) {
+	e.preventDefault();
+	sendIt();
+});
 
+function sendIt(e) {
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    const messageValue = message.value.trim();
+    const mailFormat = /([a-zA-Z0-9]+)([\_\.\-{1}])?([a-zA-Z0-9]+)\@([a-zA-Z0-9]+)([\.])([a-zA-Z\.]+)/g;
+
+    if(nameValue === '' || emailValue === '' || messageValue === '') {
+        // alert('Please fill in the fields')
+        msg.className = 'alert alert-danger';
+        msg.innerHTML = 'Please fill in all fields';
+        return false;
+
+    } else {
+        if(nameValue.length < 3){
+            msg.className = 'alert alert-danger';
+            msg.innerHTML = 'Name must consist of at least three characters.';
+            return false;
+        } else {
+            if (!emailValue.match(mailFormat)) {
+                msg.className = 'alert alert-danger';
+                msg.innerHTML = 'Incorrect email format.';
+                return false;
+            } else {
+                if(messageValue.length < 10) {
+                    msg.className = 'alert alert-danger';
+                    msg.innerHTML = 'Message has to be at least 10 characters long.';
+                    return false;
+                }else {
+                    msg.className = 'alert alert-success';
+                    msg.innerHTML = nameValue + ' we have received your contact. Thanks!';
+                }
+            }
+        }
+        sendMail(nameValue, emailValue, messageValue);
+    }
 }
-
-
-   
